17bdc7328530c6c5b2eded5f8b0264873ecfceff
melifaro <nikita.kulagin@onefactor.com>
2023-01-24 23:25:28 +0300

    middleware auth

--- build.sbt
+++ build.sbt
@@ -1,11 +1,21 @@
 val GLOBAL_VERSION = "0.0.1"
 
 scalaVersion := "2.13.10"
+val authentikat ="com.jason-goodwin" %% "authentikat-jwt" % "0.4.5"
+
+  val core = "io.circe" %% "circe-core" % "0.14.3"
+  val generic = "io.circe" %% "circe-generic" % "0.14.3"
+  val parser = "io.circe" %% "circe-parser" % "0.14.3"
+  val jawn = "io.circe" %% "circe-jawn" % "0.14.3"
+
+  val all = Seq(core, generic, parser, jawn)
 
 lazy val `zio-http-server` = (project in file("http-server-snippet"))
   .settings(Seq(name := "http-server-snippet", version := GLOBAL_VERSION))
   .settings(libraryDependencies ++= Seq(
     "dev.zio"       %% "zio"            % "2.0.6",
     "dev.zio"       %% "zio-json"       % "0.4.2",
-    "io.d11"        %% "zhttp"          % "2.0.0-RC10"
-    ))
+    "io.d11"        %% "zhttp"          % "2.0.0-RC10",
+    authentikat,
+      "com.github.jwt-scala" %% "jwt-circe" % "9.1.2"
+    ) ++ all)
--- http-server-snippet/src/main/scala/com/github/melifaro/MainApp.scala
+++ http-server-snippet/src/main/scala/com/github/melifaro/MainApp.scala
@@ -3,6 +3,7 @@ package com.github.melifaro
 import zhttp.service.Server
 import zio._
 import zhttp.http._
+import zhttp.http.middleware.HttpMiddleware
 
 object MainApp extends ZIOAppDefault {
   def run: ZIO[Environment with ZIOAppArgs with Scope, Any, Any] =
@@ -11,6 +12,35 @@ object MainApp extends ZIOAppDefault {
       http = Http.collect[Request] {
         case Method.GET -> !! / "greet" / name =>
           Response.text(s"Hello $name!")
-      }
+      }  @@ JwtTokenMiddleware()
     )
+
+}
+
+object JwtTokenMiddleware {
+  import pdi.jwt._
+  import pdi.jwt.algorithms.JwtHmacAlgorithm
+  import io.circe.jawn.decode
+  import io.circe._, io.circe.generic.semiauto._
+  import scala.util.{Failure, Success}
+
+  implicit val userTokenDecoder: Decoder[UserToken] = deriveDecoder[UserToken]
+  implicit val userTokenEncoder: Encoder[UserToken] = deriveEncoder[UserToken]
+  val ALGORITHM: JwtHmacAlgorithm = JwtAlgorithm.HS256
+  private val key = "secret-key"
+  case class UserToken(uid: String, uname: String, roles: Seq[String])
+
+  def readToken(str: String): ZIO[Any, Throwable, Boolean] = {
+    val token = for {
+      claim <- JwtCirce.decode(str, key, Seq(ALGORITHM), JwtOptions.DEFAULT)
+      info <- decode[UserToken](claim.content) match {
+        case Right(cl) => Success(cl)
+        case Left(er) => Failure(new Exception(er.getMessage))
+      }
+    } yield info
+
+    ZIO.fromTry(token.map(_ => true)).provide(ZLayer.empty)
+  }
+
+  def apply(): HttpMiddleware[Any, Nothing] = Middleware.bearerAuthZIO(readToken(_))
 }
